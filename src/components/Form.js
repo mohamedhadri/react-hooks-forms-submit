8e99df2cc6657075fdc8fb231c820e8ec418b51f
Mohamed Hadri <mhadri1000@gmail.com>
2022-07-04 13:02:04 +0300

    react form

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,7 +13,7 @@ function Form(props) {
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
